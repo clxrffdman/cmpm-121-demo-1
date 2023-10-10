29eb0b64f4bcad0f381c054e9917c7ffd020ff29
Calex Raffield <clxrffdman@gmail.com>
2023-10-10 14:54:47 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,25 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 
-const mainButton = document.querySelector("#mainClickButton");
+const clickDisplay = document.createElement("div");
+clickDisplay.innerHTML = "0 clicks!";
+let counter: number = 0;
+
+const mainButton = document.createElement("button");
+mainButton.innerHTML = "🐵";
 if (mainButton) {
   mainButton.addEventListener("click", () => mainClick());
 }
 
 app.append(header);
+app.append(clickDisplay);
+app.append(mainButton);
 
 function mainClick() {
-  console.log("clicked!");
+  counter++;
+  updateCounterDisplay();
+}
+
+function updateCounterDisplay() {
+  clickDisplay.innerHTML = counter + " clicks!";
 }
