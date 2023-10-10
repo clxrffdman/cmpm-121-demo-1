1039798a9fad5bd2c6091258cfffe47b48c7c869
Calex Raffield <clxrffdman@gmail.com>
2023-10-09 23:40:16 -0700

    added monkey button

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,14 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+const mainButton = document.querySelector("#mainClickButton");
+if (mainButton) {
+  mainButton.addEventListener("click", () => mainClick());
+}
+
 app.append(header);
+
+function mainClick() {
+  console.log("clicked!");
+}
